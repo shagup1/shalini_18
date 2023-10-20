870a696262150e63a3ba8749f7f7857221e13bdb
shagup1 <91088477+shagup1@users.noreply.github.com>
2023-10-20 20:54:34 +0530

    Add files via upload

---
+++ assignment_day1.1_solutions.java
@@ -0,0 +1,158 @@
+
+//addition of two numbers
+/*
+import java.util.Scanner;
+class addition{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter first number:");
+int a=sc.nextInt();
+System.out.println("enter second number:");
+int b=sc.nextInt();
+int sum =a+b;
+System.out.println(sum);
+
+}
+}
+*/
+
+//max between two numbers
+
+/*
+import java.util.Scanner;
+public class first {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter first number");
+		int a=sc.nextInt();
+		
+		System.out.println("enter second number");
+		int b=sc.nextInt();
+		if(a>b) {
+			System.out.println("max="+a);
+
+		}
+		else if(a<b) {
+			System.out.println("max="+b);
+
+		}
+		else {
+			System.out.println("both are equal");
+		}
+		
+
+	}
+
+}
+*/
+
+//number divisible by 3
+
+/*
+import java.util.Scanner;
+public class first {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter first number");
+		int a=sc.nextInt();
+		
+		
+		if(a%3==0) {
+			System.out.println("number is divisible by 3");
+
+		}
+		
+		else {
+			System.out.println("not divisible");
+		}
+		
+
+	}
+
+}*/
+
+//print table of given number
+
+/*
+import java.util.Scanner;
+public class table
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+
+System.out.println("enter number to print table:");
+int num=sc.nextInt();
+System.out.println("enter range:");
+//int range=sc.nextInt();
+int i=1;
+while(i<=10){
+System.out.println(num+"*"+i+"="+num*i);
+i++;
+}}}
+
+*/
+
+//subtraction of two number
+
+
+
+/*
+import java.util.Scanner;
+class addition{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter first number:");
+int a=sc.nextInt();
+System.out.println("enter second number:");
+int b=sc.nextInt();
+int sub =a-b;
+System.out.println(sub);
+
+}
+}*/
+
+//multiplication of two number
+
+
+
+
+/*
+import java.util.Scanner;
+class addition{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter first number:");
+int a=sc.nextInt();
+System.out.println("enter second number:");
+int b=sc.nextInt();
+int ans=a*b;
+System.out.println(ans);
+
+}
+}*/
+
+//division
+
+
+ 
+/*
+import java.util.Scanner;
+class addition{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter first number:");
+int a=sc.nextInt();
+System.out.println("enter second number:");
+int b=sc.nextInt();
+int ans =a/b;
+System.out.println(ans);
+
+}
+}*/ 
